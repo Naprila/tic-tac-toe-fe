3551e984090879fcd764677d2a61dbfb50452e87
Napri <manishhh821@gmail.com>
2023-12-28 21:16:26 +0530

    provided BE end-point

--- src/utils/useWebSocket.tsx
+++ src/utils/useWebSocket.tsx
@@ -4,7 +4,11 @@ const useWebSocket = () => {
   const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
 
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:3000");
+    const URL =
+      process.env.NODE_ENV === "production"
+        ? "https://tic-tac-toe-server-tre2.onrender.com"
+        : "ws://localhost:3000";
+    const ws = new WebSocket(URL);
     setWebSocket(ws);
     console.log("ws connected");
 
