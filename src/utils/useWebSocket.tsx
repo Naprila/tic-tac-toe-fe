a2d93d769904de72c6231083a3e264fe120782d9
Napri <manishhh821@gmail.com>
2023-12-28 23:34:12 +0530

    adding wss instead of https

--- src/utils/useWebSocket.tsx
+++ src/utils/useWebSocket.tsx
@@ -6,7 +6,7 @@ const useWebSocket = () => {
   useEffect(() => {
     const URL =
       process.env.NODE_ENV === "production"
-        ? "https://tic-tac-toe-server-tre2.onrender.com"
+        ? "wss://tic-tac-toe-server-tre2.onrender.com"
         : "ws://localhost:3000";
     const ws = new WebSocket(URL);
     setWebSocket(ws);
